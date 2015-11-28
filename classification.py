964c24343cd37b58ca48c3b5c97e4fbeec0fb5d4
Vishesh Gupta <vishesh@stanford.edu>
2015-11-28 14:28:07 -0800

    switched gmm classifier to doing command line based classification and now have T/F +/- type result output

--- classification.py
+++ classification.py
@@ -21,18 +21,14 @@ for filename in sys.argv[1:]:
 
 gmms = []
 for l in langs:
-  g = GMM(n_components = 2, covariance_type='full')
+  g = GMM(n_components = 4, covariance_type='full')
   g.fit(l)
   gmms.append(g)
 
 all_data = np.row_stack(langs)
-print len(all_data)
 for i in xrange(len(gmms)):
   g = gmms[i]
   l = langs[i]
-  print len(l)
   pred = [x > math.log(0.5) for x in g.score(all_data)]
   expected = [np.any(np.equal(l,x).all(1)) for x in all_data]
-  print pred
-  print expected
-
+  print t.frequencies(expected), t.frequencies(zip(expected,pred))
